078da96b152d6bbec3db1f5003205d61d4b72950
ente78 <britta.ente@gmail.com>
2017-05-30 19:11:58 +0200

    asjlkasdjf

--- frontend-nanodegree-mobile-portfolio-master/views/js/main.js
+++ frontend-nanodegree-mobile-portfolio-master/views/js/main.js
@@ -425,20 +425,31 @@ var resizePizzas = function(size) {
   function determineDx (elem, size) {
     var oldWidth = elem.offsetWidth;
     var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
+
     var oldSize = oldWidth / windowWidth;
 
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
+    // new function for changepizzasize now layout calling each time. 
+    function changePizzaSizes (size) {
       switch(size) {
         case "1":
-          return 0.25;
+          newWidth: 25;
+          break;
         case "2":
-          return 0.3333;
+          newWidth: 33.33;
+          break;
         case "3":
-          return 0.5;
+          newWidth 50;
+          break; 
         default:
           console.log("bug in sizeSwitcher");
       }
+
+      var randomPizzas = document.querySelectorAll(".randomPizzaContainer"):
+
+      for (var i=0; i < randomPizzas.length; i++){
+        randomPizzas[i].style.with = newWidth + "%";
+      }
+
     }
 
     var newSize = sizeSwitcher(size);
@@ -447,14 +458,7 @@ var resizePizzas = function(size) {
     return dx;
   }
 
-  // Iterates through pizza elements on the page and changes their widths
-  function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
-    }
-  }
+
 
   changePizzaSizes(size);
 
