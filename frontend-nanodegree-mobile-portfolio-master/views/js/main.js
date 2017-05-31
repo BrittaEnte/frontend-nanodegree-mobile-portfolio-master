69b6ab477cf3a4bf2f34ef807ece4b160c984458
ente78 <britta.ente@gmail.com>
2017-05-31 21:45:07 +0200

    12

--- frontend-nanodegree-mobile-portfolio-master/views/js/main.js
+++ frontend-nanodegree-mobile-portfolio-master/views/js/main.js
@@ -352,8 +352,7 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
   console.log("Average scripting time to generate last 10 frames: " + sum / 10 + "ms");
 }
 
-// The following code for sliding background pizzas was pulled from Ilya's demo found at:
-// https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
+
 
 // Moves the sliding background pizzas based on scroll position. dev told me that here was the problem. 
 function updatePositions() {
